1273b343c7507fe80eef0254b8abe7161e536b8b
Jose Manuel <jmdcarq89@gmail.com>
2021-04-06 19:10:14 +0200

    gatsby init

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: "Portfolio",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "ln7xcgZG5s0mIfhXERFvoh05oM1VWYAb6M4kXbPu00w",
+        spaceId: "8urwtin6d2ax",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
