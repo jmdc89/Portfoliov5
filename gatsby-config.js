05f37a9a34953f46965e8561d301d3ee7072f244
Jose Manuel <jmdcarq89@gmail.com>
2021-04-06 19:33:34 +0200

    Global styles - fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
     title: "Portfolio",
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "ln7xcgZG5s0mIfhXERFvoh05oM1VWYAb6M4kXbPu00w",
-        spaceId: "8urwtin6d2ax",
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-contentful",
+    //   options: {
+    //     accessToken: "ln7xcgZG5s0mIfhXERFvoh05oM1VWYAb6M4kXbPu00w",
+    //     spaceId: "8urwtin6d2ax",
+    //   },
+    // },
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
---
+++ src/styles/styles.js
@@ -0,0 +1,14 @@
+import { css } from "styled-components";
+
+export const setColor = {
+  primaryColor: "#af9a7d",
+  mainWhite: "#fff",
+  mainBlack: "#222",
+  mainGrey: "#ececec",
+  lightGrey: "#f7f7f7",
+};
+
+export const setFont = {
+  main: "font-family: 'Roboto', sans-serif;",
+  //   slanted: "font-family: 'Courgette', cursive;",
+};
