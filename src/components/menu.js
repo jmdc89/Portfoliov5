3cef44d7a3c77cbd75ee23939d0f3ec5cfb97214
Jose Manuel <jmdcarq89@gmail.com>
2021-04-07 11:11:42 +0200

    Styled SideBar

--- src/components/menu.js
+++ src/components/menu.js
@@ -81,3 +81,100 @@ const StyledHamburgerButton = styled.button`
     }
   }
 `;
+
+const StyledSidebar = styled.aside`
+  display: none;
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    padding: 50px 10px;
+    width: min(75vw, 400px);
+    height: 100vh;
+    outline: 0;
+    background-color: var(--light-navy);
+    box-shadow: -10px 0px 30px -15px var(--navy-shadow);
+    z-index: 9;
+    transform: translateX(${(props) => (props.menuOpen ? 0 : 100)}vw);
+    visibility: ${(props) => (props.menuOpen ? "visible" : "hidden")};
+    transition: var(--transition);
+  }
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    flex-direction: column;
+    color: var(--lightest-slate);
+    font-family: var(--font-mono);
+    text-align: center;
+  }
+  ol {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    width: 100%;
+    li {
+      position: relative;
+      margin: 0 auto 20px;
+      counter-increment: item 1;
+      font-size: clamp(var(--fz-sm), 4vw, var(--fz-lg));
+      @media (max-width: 600px) {
+        margin: 0 auto 10px;
+      }
+      &:before {
+        content: "0" counter(item) ".";
+        display: block;
+        margin-bottom: 5px;
+        color: var(--green);
+        font-size: var(--fz-sm);
+      }
+    }
+    a {
+      display: inline-block;
+      text-decoration: none;
+      text-decoration-skip-ink: auto;
+      color: inherit;
+      position: relative;
+      transition: var(--transition);
+      cursor: pointer;
+      &:hover,
+      &:active,
+      &:focus {
+        color: var(--green);
+        outline: 0;
+      }
+      width: 100%;
+      padding: 3px 20px 20px;
+    }
+  }
+  .resume-link {
+    color: var(--green);
+    background-color: transparent;
+    border: 1px solid var(--green);
+    border-radius: var(--border-radius);
+    padding: 1.25rem 1.75rem;
+    font-size: var(--fz-sm);
+    /* font-family: var(--font-mono); */
+    line-height: 1;
+    text-decoration: none;
+    cursor: pointer;
+    transition: var(--transition);
+    &:hover,
+    &:focus,
+    &:active {
+      background-color: var(--green-tint);
+      outline: none;
+    }
+    &:after {
+      display: none !important;
+    }
+    padding: 18px 50px;
+    margin: 10% auto 0;
+    width: max-content;
+  }
+`;
