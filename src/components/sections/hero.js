2b49fd1fe069733b2a073b07c1f2adf8ebbfc299
Jose Manuel <jmdcarq89@gmail.com>
2021-04-07 17:19:56 +0200

    global styles

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -2,30 +2,83 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { email } from "../../config";
 
-const Hero = () => {
-  const one = <h1>Hi. my name is</h1>;
-  const two = <h2 className="big-heading">Jose M. Dominguez</h2>;
-  const three = (
-    <h3 className="big-heading">First I drink coffee, then I build things.</h3>
-  );
-  const four = (
-    <p>
-      I'm a Spain-based architect and software developer with 10 years of
-      professional experience in digital fabrication, parametric modeling and
-      complex geometry. Currently, I´m focused on designing and building
-      exceptional digital experiences.
-    </p>
-  );
-  const five = (
-    <a href={`mailto:${email}`} className="email-link">
-      Get In Touch
-    </a>
-  );
+const StyledHeroSection = styled.section`
+  margin: 0px auto;
+  max-width: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  padding: 0;
+  @media (max-width: 480px) and (min-height: 700px) {
+    padding-bottom: 10vh;
+  }
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    /* font-family: var(--font-mono); */
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+  h3 {
+    margin-top: 10px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+  p {
+    margin: 20px 0 0;
+    max-width: 500px;
+    color: var(light-slate);
+  }
+  .email-link {
+    margin-top: 50px;
+    color: var(--green);
+    background-color: transparent;
+    border: 1px solid var(--green);
+    border-radius: var(--border-radius);
+    padding: 1.25rem 1.75rem;
+    font-size: var(--fz-sm);
+    /* font-family: var(--font-mono); */
+    line-height: 1;
+    text-decoration: none;
+    cursor: pointer;
+    transition: var(--transition);
+    &:hover,
+    &:focus,
+    &:active {
+      background-color: var(--green-tint);
+      outline: none;
+    }
+    &:after {
+      display: none !important;
+    }
+  }
+`;
 
+const Hero = () => {
   return (
     <>
-      <h1>hello form hero</h1>
-      <h2>bye from hero</h2>
+      <StyledHeroSection>
+        <h1>Hi. my name is</h1>
+        <h2 className="big-heading">Jose M. Dominguez</h2>
+        <h3 className="big-heading">
+          First I drink coffee, then I build things.
+        </h3>
+        <p>
+          I'm a Spain-based architect and software developer with 10 years of
+          professional experience in digital fabrication, parametric modeling
+          and complex geometry. Currently, I´m focused on designing and building
+          exceptional digital experiences.
+        </p>
+        <a href={`mailto:${email}`} className="email-link">
+          Get In Touch
+        </a>
+      </StyledHeroSection>
     </>
   );
 };
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -18,15 +18,14 @@ const Globals = createGlobalStyle`
         --green: #64ffda;
         --green-tint: rgba(100, 255, 218, 0.1);
 
-        --fz-xxs: 14px;
-        --fz-xs: 15px;
-        --fz-sm: 16px;
-        --fz-md: 18px;
-        --fz-lg: 20px;
-        --fz-xl: 22px;
-        --fz-xxl: 24px;
+        --fz-xxs: 12px;
+        --fz-xs: 13px;
+        --fz-sm: 14px;
+        --fz-md: 16px;
+        --fz-lg: 18px;
+        --fz-xl: 20px;
+        --fz-xxl: 22px;
         --fz-heading: 32px;
-
         --border-radius: 4px;
         --nav-height: 100px;
         --nav-scroll-height: 70px;
@@ -46,7 +45,7 @@ const Globals = createGlobalStyle`
 
     }
 
-    a{
+    /* a{
         text-decoration: none;
     }
 
@@ -57,11 +56,506 @@ const Globals = createGlobalStyle`
     }
 
     body {
+        min-height: 100%;
+        width: 100%;
         font-size: 100%;        
-        color:var(--white);
         background:var(--dark-navy);
         ${setFont.main};
+    } */
+
+    html {
+    box-sizing: border-box;
+    width: 100%;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  ::selection {
+    background-color: var(--lightest-navy);
+    color: var(--lightest-slate);
+  }
+
+  :focus {
+    outline: 2px dashed var(--green);
+    outline-offset: 3px;
+  }
+
+  body {
+    margin: 0;
+    width: 100%;
+    min-height: 100%;
+    overflow-x: hidden;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    background-color: var(--navy);
+    color: var(--slate);
+    /* font-family: var(--font-sans); */
+    ${setFont.main};
+    font-size: var(--fz-xl);
+    line-height: 1.3;
+
+    @media (max-width: 480px) {
+      font-size: var(--fz-lg);
     }
+
+    &.hidden {
+      overflow: hidden;
+    }
+
+    &.blur {
+      overflow: hidden;
+
+      header {
+        background-color: transparent;
+      }
+
+      #content > * {
+        filter: blur(5px) brightness(0.7);
+        transition: var(--transition);
+        pointer-events: none;
+        user-select: none;
+      }
+    }
+  }
+
+  #root {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: 1fr auto;
+    grid-template-columns: 100%;
+  }
+
+  main {
+    margin: 0 auto;
+    width: 100%;
+    max-width: 1600px;
+    min-height: 100vh;
+    padding: 200px 150px;
+
+    @media (max-width: 1080px) {
+      padding: 200px 100px;
+    }
+    @media (max-width: 768px) {
+      padding: 150px 50px;
+    }
+    @media (max-width: 480px) {
+      padding: 125px 25px;
+    }
+
+    &.fillHeight {
+      padding: 0 150px;
+
+      @media (max-width: 1080px) {
+        padding: 0 100px;
+      }
+      @media (max-width: 768px) {
+        padding: 0 50px;
+      }
+      @media (max-width: 480px) {
+        padding: 0 25px;
+      }
+    }
+  }
+
+  section {
+    margin: 0 auto;
+    padding: 100px 0;
+    max-width: 1000px;
+
+    @media (max-width: 768px) {
+      padding: 80px 0;
+    }
+
+    @media (max-width: 480px) {
+      padding: 60px 0;
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0 0 10px 0;
+    font-weight: 600;
+    color: var(--lightest-slate);
+    line-height: 1.1;
+  }
+
+  .big-heading {
+    margin: 0;
+    font-size: clamp(40px, 8vw, 80px);
+  }
+
+  .medium-heading {
+    margin: 0;
+    font-size: clamp(40px, 8vw, 60px);
+  }
+
+  .numbered-heading {
+    display: flex;
+    align-items: center;
+    position: relative;
+    margin: 10px 0 40px;
+    width: 100%;
+    font-size: clamp(26px, 5vw, var(--fz-heading));
+    white-space: nowrap;
+
+    &:before {
+      position: relative;
+      bottom: 4px;
+      counter-increment: section;
+      content: '0' counter(section) '.';
+      margin-right: 10px;
+      color: var(--green);
+      font-family: var(--font-mono);
+      font-size: clamp(var(--fz-md), 3vw, var(--fz-xl));
+      font-weight: 400;
+
+      @media (max-width: 480px) {
+        margin-bottom: -3px;
+        margin-right: 5px;
+      }
+    }
+
+    &:after {
+      content: '';
+      display: block;
+      position: relative;
+      top: -5px;
+      width: 300px;
+      height: 1px;
+      margin-left: 20px;
+      background-color: var(--lightest-navy);
+
+      @media (max-width: 1080px) {
+        width: 200px;
+      }
+      @media (max-width: 768px) {
+        width: 100%;
+      }
+      @media (max-width: 600px) {
+        margin-left: 10px;
+      }
+    }
+  }
+
+  img,
+  svg,
+  .gatsby-image-wrapper {
+    width: 100%;
+    max-width: 100%;
+    vertical-align: middle;
+  }
+
+  img[alt=""],
+  img:not([alt]) {
+    filter: blur(5px);
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: currentColor;
+    vertical-align: middle;
+
+    &.feather {
+      fill: none;
+    }
+  }
+
+  a {
+    display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    color: inherit;
+    position: relative;
+    transition: var(--transition);
+    cursor: pointer;
+
+    &:hover,
+    &:focus {
+      color: var(--green);
+    }
+
+    &.inline-link {
+      display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    position: relative;
+    transition: var(--transition);
+    cursor: pointer;
+    color: var(--green);
+    &:hover,
+    &:focus,
+    &:active {
+      color: var(--green);
+      outline: 0;
+      &:after {
+        width: 100%;
+      }
+      & > * {
+        color: var(--green) !important;
+        transition: var(--transition);
+      }
+    }
+    &:after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 1px;
+      position: relative;
+      bottom: 0.37em;
+      background-color: var(--green);
+      transition: var(--transition);
+      opacity: 0.5;
+    }
+    }
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
+    border-radius: 0;
+  }
+
+  input, textarea {
+    border-radius: 0;
+    outline: 0;
+
+    &:focus {
+      outline: 0;
+    }
+    &:focus,
+    &:active {
+      &::placeholder {
+        opacity: 0.5;
+      }
+    }
+  }
+
+  p {
+    margin: 0 0 15px 0;
+
+    &:last-child,
+    &:last-of-type {
+      margin: 0;
+    }
+
+    & > a {
+      display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    position: relative;
+    transition: var(--transition);
+    cursor: pointer;
+    color: var(--green);
+    &:hover,
+    &:focus,
+    &:active {
+      color: var(--green);
+      outline: 0;
+      &:after {
+        width: 100%;
+      }
+      & > * {
+        color: var(--green) !important;
+        transition: var(--transition);
+      }
+    }
+    &:after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 1px;
+      position: relative;
+      bottom: 0.37em;
+      background-color: var(--green);
+      transition: var(--transition);
+      opacity: 0.5;
+    }
+    }
+
+    & > code {
+      background-color: var(--light-navy);
+      color: var(--white);
+      font-size: var(--fz-sm);
+      border-radius: var(--border-radius);
+      padding: 0.3em 0.5em;
+    }
+  }
+
+  ul {
+    &.fancy-list {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      font-size: var(--fz-lg);
+      li {
+        position: relative;
+        padding-left: 30px;
+        margin-bottom: 10px;
+        &:before {
+          content: '▹';
+          position: absolute;
+          left: 0;
+          color: var(--green);
+        }
+      }
+    }
+  }
+
+  blockquote {
+    border-left-color: var(--green);
+    border-left-style: solid;
+    border-left-width: 1px;
+    margin-left: 0px;
+    margin-right: 0px;
+    padding-left: 1.5rem;
+
+    p {
+      font-style: italic;
+      font-size: 24px;
+    }
+  }
+
+  hr {
+    background-color: var(--lightest-navy);
+    height: 1px;
+    border-width: 0px;
+    border-style: initial;
+    border-color: initial;
+    border-image: initial;
+    margin: 1rem;
+  }
+
+  code {
+    font-family: var(--font-mono);
+    font-size: var(--fz-md);
+  }
+
+  #logo {
+    color: var(--green);
+  }
+
+  .overline {
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: var(--fz-md);
+    font-weight: 400;
+  }
+
+  .subtitle {
+    color: var(--green);
+    margin: 0 0 20px 0;
+    font-size: var(--fz-md);
+    font-family: var(--font-mono);
+    font-weight: 400;
+    line-height: 1.5;
+    @media (max-width: 1080px) {
+      font-size: var(--fz-sm);
+    }
+    @media (max-width: 768px) {
+      font-size: var(--fz-xs);
+    }
+
+    a {
+      display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    position: relative;
+    transition: var(--transition);
+    cursor: pointer;
+    color: var(--green);
+    &:hover,
+    &:focus,
+    &:active {
+      color: var(--green);
+      outline: 0;
+      &:after {
+        width: 100%;
+      }
+      & > * {
+        color: var(--green) !important;
+        transition: var(--transition);
+      }
+    }
+    &:after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 1px;
+      position: relative;
+      bottom: 0.37em;
+      background-color: var(--green);
+      transition: var(--transition);
+      opacity: 0.5;
+    }
+      line-height: 1.5;
+    }
+  }
+
+  .breadcrumb {
+    display: flex;
+    align-items: center;
+    margin-bottom: 50px;
+    color: var(--green);
+
+    .arrow {
+      display: block;
+      margin-right: 10px;
+      padding-top: 4px;
+    }
+
+    a {
+      display: inline-block;
+    text-decoration: none;
+    text-decoration-skip-ink: auto;
+    position: relative;
+    transition: var(--transition);
+    cursor: pointer;
+    color: var(--green);
+    &:hover,
+    &:focus,
+    &:active {
+      color: var(--green);
+      outline: 0;
+      &:after {
+        width: 100%;
+      }
+      & > * {
+        color: var(--green) !important;
+        transition: var(--transition);
+      }
+    }
+    &:after {
+      content: '';
+      display: block;
+      width: 0;
+      height: 1px;
+      position: relative;
+      bottom: 0.37em;
+      background-color: var(--green);
+      transition: var(--transition);
+      opacity: 0.5;
+    }
+      font-family: var(--font-mono);
+      font-size: var(--fz-sm);
+      font-weight: 600;
+      line-height: 1.5;
+      text-transform: uppercase;
+      letter-spacing: 0.1em;
+    }
+  }
+
+  .gatsby-image-outer-wrapper {
+    height: 100%;
+  }
 
 
 `;
