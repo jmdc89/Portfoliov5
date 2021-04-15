c505304d91655df9d6f303839a829d941658b2cf
Jose Manuel <jmdcarq89@gmail.com>
2021-04-15 22:01:52 +0200

    images query

--- src/components/sections/sfjob.js
+++ src/components/sections/sfjob.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+
+const query = graphql`
+  {
+    sfPhotos: allFile(filter: { relativeDirectory: { eq: "sf" } }) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH, quality: 10)
+        }
+      }
+    }
+  }
+`;
 
 const Sfjob = () => {
+  const data = useStaticQuery(query);
+  console.log(data);
+
   return (
     <>
       <h1>photo</h1>
       <h2>bye</h2>
+      {data.sfPhotos.nodes.map((image) => (
+        <GatsbyImage image={image.childImageSharp.gatsbyImageData} />
+      ))}
     </>
   );
 };
